e4400a1dd0683cef0aab6652a355f1369dee7119
Andrea Monroy <andrearamonroy@gmail.com>
2022-11-29 09:38:29 -0500

    nCHAR

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ db.init_app(app)
     
 class Level(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    level_name = db.Column(db.NVARCHAR(150))
+    level_name = db.Column(db.NCHAR(150))
     #evel_description = db.Column(db.String)
     units = db.relationship('Unit', backref='level')
 
